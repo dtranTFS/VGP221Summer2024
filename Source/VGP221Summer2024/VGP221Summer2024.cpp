b129ce1912e18b021907c1c768eac01acbfd0c28
Danny Tran <dtran@torontofilmschool.ca>
2024-07-10 19:50:08 -0400

    Empty Starter Project
    Summary:
    - Empty c++ project with gamemode to test

---
+++ Source/VGP221Summer2024/VGP221Summer2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VGP221Summer2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VGP221Summer2024, "VGP221Summer2024" );
