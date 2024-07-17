728eb73d15187f9cce1f109637eb13ef1522d2b1
Danny Tran <dtran@torontofilmschool.ca>
2024-07-17 19:56:42 -0400

    Unreal Tutorial Part 1
    Summary:
    - Implemented the first part of the UE5 C++ FPS Tutorial

--- Source/VGP221Summer2024/FPSGameMode.cpp
+++ Source/VGP221Summer2024/FPSGameMode.cpp
@@ -11,5 +11,14 @@ void AFPSGameMode::StartPlay()
 
     // Display a debug message for five seconds. 
     // The -1 "Key" value argument prevents the message from being updated or refreshed.
-    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode!"));
+    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Blue, TEXT("Hello World, this is FPSGameMode!"));
+
+    // Debugging in the console
+    UE_LOG(LogTemp, Warning, TEXT("Hello World, this is FPSGameMode!"));
+
+    int test = 10;
+    bool boolTest = true;
+    //UE_LOG(LogTemp, Warning, TEXT("Test: %i, TestBool: %d, StringText: %s"), test, boolTest, TEXT("Test"));
+
+    //UE_LOGFMT(LogTemp, Warning, "Test: {test}, boolTest: {boolTest}", test, boolTest);
 }
---
+++ Source/VGP221Summer2024/Private/Player/FPSCharacter.cpp
@@ -0,0 +1,101 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Similar Unity Instantiate but only for constructors
+	if (!FPSCameraComponent) {
+		FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+		FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+		FPSCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+		FPSCameraComponent->bUsePawnControlRotation = true;
+	}
+
+	if (!FPSMesh) {
+		FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+		FPSMesh->SetupAttachment(FPSCameraComponent);
+		FPSMesh->bCastDynamicShadow = false;
+		FPSMesh->CastShadow = false;
+	}
+
+	GetMesh()->SetOwnerNoSee(true);
+
+	UE_LOG(LogTemp, Warning, TEXT("Character Constructor Is Being Called"));
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRightNotTheSameAsInput);
+
+	// Look
+	PlayerInputComponent->BindAxis("LookHorizontal", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookVertical", this, &AFPSCharacter::AddControllerPitchInput);
+
+	// Jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPSCharacter::EndJump);
+
+	// Fire
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
+}
+
+void AFPSCharacter::MoveForward(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, value);
+
+	// Forward Vector from actor also works. Based on documentation
+	//GetActorForwardVector();
+}
+
+void AFPSCharacter::MoveRightNotTheSameAsInput(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+
+	// In blueprint we used Jump() so we can also use it here based on the documentation
+	//Jump();
+}
+
+void AFPSCharacter::EndJump()
+{
+	bPressedJump = false;
+}
+
+void AFPSCharacter::Fire()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Shooting"));
+	UE_LOG(LogTemp, Warning, TEXT("Shooting"));
+}
+
+
