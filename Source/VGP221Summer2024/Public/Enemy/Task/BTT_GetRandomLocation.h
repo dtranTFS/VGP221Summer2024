09ffb020343a909bfcde76bb5de43543b2cf2637
Danny Tran <dtran@torontofilmschool.ca>
2024-08-21 17:54:19 -0400

    Setup Enemy AI Classes
    Summary:
    - New Enemy AI Character class
    - New Enemy AI controller class
    - New task for Attack and GetRandomLocation

---
+++ Source/VGP221Summer2024/Public/Enemy/Task/BTT_GetRandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_GetRandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VGP221SUMMER2024_API UBTT_GetRandomLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
